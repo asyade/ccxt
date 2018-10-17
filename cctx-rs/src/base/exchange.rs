f10624474fabcec06f3dd393508b62b1732b2fb5
asyade <acorbeau@student.42.fr>
2018-10-17 22:41:29 +0200

    Add doc

--- cctx-rs/src/base/exchange.rs
+++ cctx-rs/src/base/exchange.rs
@@ -13,42 +13,6 @@ use serde_json;
 // pub const USER_AGENTS_CHROME: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36";
 // pub const USER_AGENT_CHROME39: &str = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36";
 // 
-// pub struct ApiMetaInfos {
-    // publicAPI: bool,
-    // privateAPI: bool,
-    // CORS: bool,
-    // cancelOrder: bool,
-    // cancelOrders: bool,
-    // createDepositAddress: bool,
-    // createOrder: bool,
-    // createMarketOrder: bool,
-    // createLimitOrder: bool,
-    // deposit: bool,
-    // editOrder: bool,
-    // fetchBalance: bool,
-    // fetchClosedOrders: bool,
-    // fetchCurrencies: bool,
-    // fetchDepositAddress: bool,
-    // fetchDeposits: bool,
-    // fetchFundingFees: bool,
-    // fetchL2OrderBook: bool,
-    // fetchMarkets: bool,
-    // fetchMyTrades: bool,
-    // fetchOHLCV: bool,
-    // fetchOpenOrders: bool,
-    // fetchOrder: bool,
-    // fetchOrderBook: bool,
-    // fetchOrderBooks: bool,
-    // fetchOrders: bool,
-    // fetchTicker: bool,
-    // fetchTickers: bool,
-    // fetchTrades: bool,
-    // fetchTradingFees: bool,
-    // fetchTradingLimits: bool,
-    // fetchTransactions: bool,
-    // fetchWithdrawals: bool,
-    // withdraw: bool,
-//} 
 
 ///
 /// The generic Exchange wrapper
@@ -70,13 +34,22 @@ pub trait ExchangeTrait {
 /// For now there is only an http connector but we can add more like WebSocket connector
 /// 
 
+///
+/// Http request body (will be json)
+/// 
 pub trait RequestBody {}
 
+///
+/// Http request parametter
+/// 
 pub struct RequestParam<'a> {
     key: &'a str,
     value: &'a str,
 }
 
+///
+/// Http request type enum with is parametters
+/// 
 pub enum RequestMethod<'a> {
     Get(Vec<RequestParam<'a>>),
     Post(Vec<RequestParam<'a>>, &'a RequestBody),
@@ -87,18 +60,34 @@ pub struct Request<'a> {
     pub methode: RequestMethod<'a>
 }
 
+///
+/// Http request response value (Parsed json)
+/// @TODO maybe do not parse the json and parse it later strongely typed
+/// 
 pub enum RequestResponse {
     Error,
     Success(Value),
 }
 
-
 pub type ConnectorFuture<T> = Box<Future<Item=T, Error=Error>>;
+
 pub trait Connector {
     fn request(&self, request: Request) -> Result<ConnectorFuture<RequestResponse>, Error>;
 }
 
+///
+/// @TODO
+/// 
 pub struct Credentials {}
+
+
+///
+/// 
+/// Echange api base
+/// TODO doc
+/// 
+/// 
+
 
 #[derive(Debug)]
 pub enum ExchangeApiRoute {
@@ -146,6 +135,7 @@ pub struct Exchange {
 impl Default for Exchange {
     fn default() -> Exchange{
         Exchange {
+            ///@TODO Non parsed value, will be deleted
             settings: None,
             id: String::new(),
             name: String::new(),
